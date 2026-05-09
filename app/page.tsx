43f4718c13804cdd2fea01fea801fb3b6a608a2d
unknown <mustaphas902@gmail.com>
2026-05-09 08:29:55 +0100

    new updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 // import Image from "next/image";
 import Hero from "@/components/website/Hero";
+import OurMission from "@/components/website/OurMission";
+import OurPastors from "@/components/website/ourpastorsOurPastors";
 export default function Home() {
   return (
-<div>
-<Hero/>
-</div>
+    <div>
+      <Hero />
+      <OurMission />
+      <OurPastors />
+    </div>
   );
 }
---
+++ components/website/ourpastors/Ourpastors.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ourpastors = () => {
+  return (
+    <div>Ouripastors</div>
+  )
+}
+
+export default Ourpastors

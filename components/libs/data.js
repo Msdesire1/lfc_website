af9d560f58316bd351979581e15b45e8d3c02fb2
Olatech01 <mustaphas902@gmail.com>
2026-05-18 14:44:00 +0100

    new

---
+++ components/libs/data.js
@@ -0,0 +1,77 @@
+export const infos = [
+    {
+        title: "Children's Ministry",
+        description: "It is not just a service group but also a ministry of its own: a colorful, energetic, entertaining and dynamic ministry focused on teaching the word of faith to children from age one month to 12 years.",
+        image: "/child.svg"
+    },
+    {
+        title: "Choir",
+        description: "The Music Ministry is made up of God-fearing men and women, who are talented music ministers and are committed to the work of God in music. To belong to this group, experience in singing and skill to play instruments will be an added advantage.",
+        image: "/choir.svg"
+    },
+    {
+        title: "Protocol Team",
+        description: "This team performs protocol duties during services, such as manning the church entrance, main sanctuary, and Pastor’s office. In addition they tend to visiting ministers, VIPs, and other church guests.",
+        image: "/protocol.svg"
+    },
+    {
+        title: "Drama Team",
+        description: "The mission of this team is to use theater as a ministry to encourage, instruct, and uplift the body of Christ and in the process, build up a vibrant and enthusiastic group of young people who are on fire for God.",
+        image: "/drama.svg"
+    },
+    {
+        title: "Hospitality",
+        description: "This team loves to create a friendly and welcoming atmosphere in and around our church services. Besides welcoming worshippers at the entrance of the church, and assisting those who may need help to their seats",
+        image: "/child.svg"
+    },
+    {
+        title: "Sanctuary Keepers",
+        description: "We have a team of committed men and women, devoted to making sure that the house of God is clean and tidy to ensure God’s welcoming presence in our services.",
+        image: "/sanctuary.svg"
+    },
+    {
+        title: "Security",
+        description: "The security, health and safety of all is the priority of this team. Serving military personnel, paramilitary personnel, ex-service men, and security service operators are welcome to the team.",
+        image: "/security.svg"
+    },
+    {
+        title: "Media And Production",
+        description: "This group is made up of skilled and trained media personnel who are responsible for our graphics, TV outreach programs, film editing, and all forms of electronic and print media.",
+        image: "/media.svg"
+    },
+    {
+        title: "Medical Team (The Gilead Squad)",
+        description: "Our worship services are undeniably safe at all times, and by God’s grace, we have had no incidence since inception. However, the Medical team exist to provide initial care in an unlikely case of an injury, to prevent a condition from worsening or danger of further injury.",
+        image: "/medical.svg"
+    },
+    {
+        title: "Decoration",
+        description: "The Music Ministry is made up of God-fearing men and women, who are talented music ministers and are committed to the work of God in music. To belong to this group, experience in singing and skill to play instruments will be an added advantage.",
+        image: "/decoration.svg"
+    },
+    {
+        title: "Transportation Team",
+        description: "The objective of this ministry is to provide transportation services and relieve our members, who have no alternatives transportation means, or stress related to transportation issues.",
+        image: "/transport.svg"
+    },
+    {
+        title: "Usher Team",
+        description: "Members of this team create a friendly and welcoming atmosphere in the church services. They show kindness and care for those in need of physical support, receive worshippers and usher them to their seats",
+        image: "/protocol.svg"
+    },
+    {
+        title: "Follow-up Team",
+        description: "This team dedicates itself to connecting first time guests and new converts to the church. They thrive on building strong believers by helping them take first steps in their walk with Christ.",
+        image: "/decoration.svg"
+    },
+    {
+        title: "Parking Team",
+        description: "This team ensures safety from the moment guests pull onto our property. They keep the parking lot organized and accommodate our congretation in many ways.",
+        image: "/parking.svg"
+    },
+    {
+        title: "Usher Team",
+        description: "Members of this team create a friendly and welcoming atmosphere in the church services. They show kindness and care for those in need of physical support, receive worshippers and usher them to their seats",
+        image: "/medical.svg"
+    },
+]
--- components/website/service-unit/Discover.jsx
+++ components/website/service-unit/Discover.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { infos } from '../../libs/data'
+import Image from 'next/image'
 
 const Discover = () => {
+  
   return (
-    <div>
-      <div>
-        <h2 className='text-[#121212] text-[40px] font-semibold w-[756px]'>
+    <div className="py-8 px-4 md:px-10 flex flex-col items-center gap-[40px] lg:px-16">
+      <div className="flex flex-col w-full">
+        <h2 className='text-[#121212] text-[40px] leading-[50px] font-semibold w-[756px]'>
           Discover the Different Ministries You Can Be Part Of
         </h2>
+        <p className='text-[#000000CC] lg:w-[625px] w-full md:[625px]'>
+          Explore a range of ministries where you can serve, grow in faith, and contribute meaningfully to the church community.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 w-full lg:grid-cols-3 gap-6">
+        {infos.map((info, idx) => (
+          <div key={idx} className="flex flex-col gap-4 p-3 rounded-lg bg-[#F5F5F5]">
+            <Image width={471} height={470} src={info.image} alt={info.title} className="object-cover" />
+            <h3 className="text-xl font-semibold text-[#121212]">{info.title}</h3>
+            <p className="text-[14px] font-normal text-[#121212]">{info.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   )

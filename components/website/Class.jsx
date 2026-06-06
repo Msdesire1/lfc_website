fc86c2e367785cad9a7f1cfa436b4d456608d66d
Adeyemo olamide <adeyemok831@gmail.com>
2026-06-06 12:22:05 +0100

    new

--- components/website/Class.jsx
+++ components/website/Class.jsx
@@ -12,16 +12,16 @@ const Class = () => {
                         src="/bfc.png"
                         alt="Believers Foundation Class"
                         width={700}
-                        height={900}
+                        height={980}
                         className="w-full h-full object-cover hover:scale-105 transition duration-500"
                     />
                 </div>
 
                 {/* Right Content */}
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
   Believers Foundation Class
                     {/* Heading */}
-                    <div className="flex flex-1 items-center gap-3">
+                    <div className="flex flex-1 items-center gap-4">
                         <h2 className="lg:text-[30px] md:text-[50px] font-bold text-[#EC3237] leading-tight">
                             Believers&apos;
                         </h2>
@@ -32,14 +32,14 @@ const Class = () => {
                     </div>
 
                     {/* Intro Text */}
-                    <p className="text-black text-[16px] font-light">
+                    <p className="text-black text-[14px] font-light">
                         The Believers&apos; Foundation Class and Membership Classes at
                         Living Faith Church (Winners Chapel) is a platform for all
                         new believers and first-time worshippers to be established
                         in the faith.
                     </p>
 
-                    <p className="text-black text-[16px] font-light">
+                    <p className="text-black text-[14px] font-light">
                         In this class, new members are inducted into the principles
                         and Christian beliefs of the ministry. Everyone is encouraged
                         to attend and become grounded in the Word of God.
@@ -55,22 +55,23 @@ const Class = () => {
                         <div className="flex flex-col gap-2">
 
                             {[
-                                "WORD FOUNDATION",
-                                "EFFECTIVE PRAYER LIFE",
-                                "KINGDOM STEWARDSHIP",
-                                "HOLY GHOST BAPTISM",
-                                 "KINGDOM STEWARDSHIP",
-                                "HOLY GHOST BAPTISM",
+                                "BE COMMITTED TO FEEDING ON THE WORD ",
+                                "THE FEAR OF THE LORD ",
+                                "THE LOVE OF GOD ",
+                                "UNDERSTANDING THE PLACE AND POWER OF FAITH ",
+                                 "UNDERSTANDING THE POWER OF PRAYER",
+                                "UNDERSTANDING THE BENEFITS OF SOUL WINNING ",
+                                "WHY EVERY BELIEVER MUST BE BAPTISED IN THE HOLY GHOST",
                             ].map((item, index) => (
                                 <div
                                     key={index}
                                     className="flex items-center gap-4 bg-white text-[12px] rounded-xl px-5 py-2 shadow-sm hover:shadow-md transition duration-300 hover:-translate-y-1"
                                 >
-                                    <div className="w-7 h-5 rounded-full bg-[#EC3237] text-white flex items-center justify-center font-semibold">
+                                    <div className="w-5 h-5 rounded-full bg-[#EC3237] text-white flex items-center justify-center font-semibold">
                                         {index + 1}
                                     </div>
 
-                                    <p className="text-[#5C5C5C] text-[18px] font-medium">
+                                    <p className="text-[#5C5C5C] text-[14px] font-medium">
                                         {item}
                                     </p>
                                 </div>
@@ -80,19 +81,12 @@ const Class = () => {
                     </div>
 
                     {/* Bottom Text */}
-                    <p className="text-black text-[16px]  font-light">
+                    <p className="text-black text-[14px]  font-light">
                         Every growth-thirsty Christian should seek to attend the
                         Believers Class. It helps to create a solid spiritual
                         foundation that is strong enough to withstand the challenges
                         of life.
                     </p>
-
-                    {/* Button */}
-                    {/* <div>
-                        <button className="bg-[#EC3237] hover:bg-[#EC3237] transition duration-300 text-white px-8 py-4 rounded-xl text-[16px] font-semibold shadow-md hover:scale-105">
-                            Join Foundation Class
-                        </button>
-                    </div> */}
 
                 </div>
             </div>

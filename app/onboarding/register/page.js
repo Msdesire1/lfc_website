f9d3f7d461211ff4e23962783ccc1a33322a2529
Adeyemo olamide <adeyemok831@gmail.com>
2026-09-05 09:57:52 +0100

    admin

--- app/onboarding/register/page.js
+++ app/onboarding/register/page.js
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { auth } from '@/lib/api';
 import { messageOf, useAction } from '@/lib/useApi';
